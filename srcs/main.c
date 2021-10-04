b2dbe029f70694cff96cbaa278b252c688c7600e
jodufour <jodufour@student.42.fr>
2021-10-04 02:18:49 +0200

    minor improve in main and strchr

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/27 21:34:22 by jodufour          #+#    #+#             */
-/*   Updated: 2021/10/02 23:23:45 by jodufour         ###   ########.fr       */
+/*   Updated: 2021/10/03 19:10:53 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,9 @@ int	main(int ac, char const **av)
 	if (ret == SUCCESS)
 		ret = mp_ctx_init(av[1]);
 	if (ret == SUCCESS)
-	{
 		ret = mp_ctx_zone_fill();
-		if (ret == SUCCESS)
-			mp_ctx_zone_print();
-	}
+	if (ret == SUCCESS)
+		mp_ctx_zone_print();
 	mp_ctx_clear();
 	if (ret != SUCCESS)
 		return (mp_err_msg(ret));
--- srcs/utils/mp_strchr.c
+++ srcs/utils/mp_strchr.c
@@ -6,7 +6,7 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 21:54:16 by jodufour          #+#    #+#             */
-/*   Updated: 2021/09/30 22:34:14 by jodufour         ###   ########.fr       */
+/*   Updated: 2021/10/04 02:12:21 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,9 @@
 
 char	*mp_strchr(char const *str, char const c)
 {
-	register char const	*ptr = str;
-
-	while (*ptr && *ptr != c)
-		++ptr;
-	if (c && !*ptr)
-		return (NULL);
-	return ((char *)ptr);
+	while (*str && *str != c)
+		++str;
+	if (*str == c)
+		return ((char *)str);
+	return (NULL);
 }
